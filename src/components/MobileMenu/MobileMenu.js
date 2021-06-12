f25d65377e1ab9c09d2615f567fdebbdfef8e5c1
Alexandre Lim <alexandre.lim94@gmail.com>
2021-06-12 17:06:55 +0200

    Mobile menu modal solution

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,43 +3,36 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { COLORS, QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, COLORS, WEIGHTS } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-        <Content>
-          <Header>
-            <CloseButton onClick={onDismiss}>
-              <VisuallyHidden>Close</VisuallyHidden>
-              <Icon id="close" strokeWidth={2} />
-            </CloseButton>
-          </Header>
-          <Nav>
-            <a href="/sale">Sale</a>
-            <a href="/new">New&nbsp;Releases</a>
-            <a href="/men">Men</a>
-            <a href="/women">Women</a>
-            <a href="/kids">Kids</a>
-            <a href="/collections">Collections</a>
-          </Nav>
-          <Footer>
-            <a href="/terms">Terms and Conditions</a>
-            <a href="/privacy">Privacy Policy</a>
-            <a href="/contact">Contact Us</a>
-          </Footer>
-        </Content>
-      </Overlay>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content aria-label="Menu">
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Filler />
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
 
@@ -49,69 +42,62 @@ const Overlay = styled(DialogOverlay)`
   left: 0;
   right: 0;
   bottom: 0;
-  background: hsla(220, 5%, 40%, 0.8);
+  background: hsl(220deg 5% 40% / 0.8);
   display: flex;
   justify-content: flex-end;
 `;
 
 const Content = styled(DialogContent)`
+  background: white;
+  width: 300px;
+  height: 100%;
+  padding: 32px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  padding-left: 32px;
-  background: ${COLORS.white};
-  width: 80%;
-  height: 100%;
-`;
-
-const Header = styled.header`
-  display: flex;
-  justify-content: flex-end;
 `;
 
 const CloseButton = styled(UnstyledButton)`
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  padding-top: 32px;
-  padding-right: 22px;
+  position: absolute;
+  top: 10px;
+  right: 0;
+  padding: 16px;
 `;
 
 const Nav = styled.nav`
   display: flex;
   flex-direction: column;
-  gap: 22px;
+  gap: 16px;
+`;
 
-  font-family: Raleway;
-  font-style: normal;
+const NavLink = styled.a`
+  color: ${COLORS.gray[900]};
   font-weight: ${WEIGHTS.medium};
-  font-size: ${18 / 16}rem;
-  line-height: 21px;
+  text-decoration: none;
+  font-size: 1.125rem;
   text-transform: uppercase;
 
-  a {
-    color: ${COLORS.gray[900]};
-    &:first-of-type {
-      color: ${COLORS.secondary};
-    }
+  &:first-of-type {
+    color: ${COLORS.secondary};
   }
 `;
 
+const Filler = styled.div`
+  flex: 1;
+`;
+
 const Footer = styled.footer`
+  flex: 1;
   display: flex;
   flex-direction: column;
+  justify-content: flex-end;
   gap: 14px;
-  padding-bottom: 32px;
+`;
 
-  font-family: Raleway;
-  font-style: normal;
+const FooterLink = styled.a`
+  color: ${COLORS.gray[700]};
   font-weight: ${WEIGHTS.normal};
+  text-decoration: none;
   font-size: ${14 / 16}rem;
-  line-height: 16px;
-
-  a {
-    color: ${COLORS.gray[700]};
-  }
 `;
 
 export default MobileMenu;
