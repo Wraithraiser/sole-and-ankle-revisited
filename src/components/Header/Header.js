5f4ac4de7df650bcdebf94acb78fbb6baf38a807
Alexandre Lim <alexandre.lim94@gmail.com>
2021-06-12 17:53:47 +0200

    Fluid desktop navigation solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,11 +12,6 @@ import VisuallyHidden from '../VisuallyHidden';
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
       <SuperHeader />
@@ -61,31 +56,24 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-  overflow-y: hidden;
-
-  @media (max-width: 1160px) {
-    overflow-x: scroll;
-  }
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
     align-items: center;
     border-top: 4px solid ${COLORS.gray[900]};
-    overflow-x: hidden;
   }
 
   @media ${QUERIES.phoneAndSmaller} {
     padding-left: 16px;
     padding-right: 16px;
-    overflow-x: hidden;
   }
 `;
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
